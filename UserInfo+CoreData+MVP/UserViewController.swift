4d952f8c89593375e2b770524f71261b55881fb6
Artem Galay <iostema@mail.ru>
2022-11-12 19:29:27 +0300

    Setup navigationController.

--- UserInfo+CoreData+MVP/UserViewController.swift
+++ UserInfo+CoreData+MVP/UserViewController.swift
@@ -8,9 +8,40 @@
 import UIKit
 
 class UserViewController: UIViewController {
+
+    private lazy var userTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Print your name here"
+        textField.backgroundColor = .systemGray6
+        textField.layer.cornerRadius = 10
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        view.backgroundColor = .white
+        setupNavigationController()
+        setupHierarchy()
+        setupLayout()
+    }
+
+    private func setupNavigationController() {
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.largeTitleDisplayMode = .always
+        navigationItem.title = "Users"
+    }
+
+    private func setupHierarchy() {
+        view.addSubview(userTextField)
+    }
+
+    private func setupLayout() {
+        NSLayoutConstraint.activate([
+            userTextField.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            userTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            userTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            userTextField.heightAnchor.constraint(equalToConstant: 50)
+        ])
     }
 }
